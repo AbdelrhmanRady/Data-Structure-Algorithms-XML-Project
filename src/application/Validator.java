8b88b2fd5085482be8f355dd18d835319e135525
liliane-atef <1900541@eng.asu.edu.eg>
2022-12-09 01:48:48 +0200

    Checking for errors

--- src/application/Validator.java
+++ src/application/Validator.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,5 +38,41 @@ public class Validator extends ArrStack{
             System.out.println("false");
             return false;
         }
+    }
+    
+    public void Fix_Error(String[] Uncorrect_File)
+    {
+        Pattern open_bracket = Pattern.compile("<");            /*writing < symbol in open_bracket*/
+        Pattern close_bracket = Pattern.compile(">");           /*writing > symbol in close_bracket*/
+        ArrayList<String> Fixed_file = new ArrayList<String>();
+        ArrStack S1 = new ArrStack();
+        String s="";
+        int counter=0;
+        for(int i=0;i<Uncorrect_File.length;i++)
+        {
+            Fixed_file.add(Uncorrect_File[i]);
+            Matcher matcher1 = open_bracket.matcher(Uncorrect_File[i]);           /*searching for < in the xml file*/
+            Matcher matcher2 = close_bracket.matcher(Uncorrect_File[i]);
+            while (matcher1.find()) {
+                if(!matcher2.find()){
+                    s=Fixed_file.get(i)+">";
+                    Fixed_file.set(i, s);
+                }
+            }
+            while (matcher2.find()) {
+                if(!matcher1.find()){
+                    s=Fixed_file.get(i);
+                    while(Fixed_file.get(i).charAt(counter) == ' '){
+                        counter++;
+                    }
+                    s=s.substring(0, counter)+"<"+s.substring(counter);
+                    Fixed_file.set(i, s);
+                }
+            }
+            System.out.println(Fixed_file.get(i));
+            counter=0;
+        }
+
+
     }
 }
